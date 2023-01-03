383be981834488e353ae831a159beed7df701a0d
romi-gomez <contact@romigomez.com.ar>
2023-01-03 17:58:55 -0300

    Chore: Fix base route in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
-    base: '/<sandbox-doggies>/'
+    base: '/sandbox-doggies/'
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
