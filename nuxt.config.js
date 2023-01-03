11a78d408b467c8955264788c3740a95d13b5189
romi-gomez <contact@romigomez.com.ar>
2023-01-03 17:48:51 -0300

    Feat: allow deploy to gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/<sandbox-doggies>/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'sandbox-doggies',
